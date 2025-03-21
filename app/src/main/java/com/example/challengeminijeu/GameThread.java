2c9e5754ea355db1b79692496f2072d3bf694b6d
aniaz <aniazatyan.2001@gmail.com>
2025-03-21 08:12:30 +0100

    initial commit

---
+++ app/src/main/java/com/example/challengeminijeu/GameThread.java
@@ -0,0 +1,61 @@
+package com.example.challengeminijeu;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread {
+
+    private SurfaceHolder surfaceHolder;
+    private GameView gameView;
+    private Canvas canvas;
+    private boolean running;
+    public GameThread(SurfaceHolder surfaceHolder, GameView gameView) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+    }
+    public void setRunning(boolean isRunning) {
+        running = isRunning;
+    }
+
+    @Override
+    public void run() {
+        long startTime;
+        long timeMillis;
+        long targetTime = 1000 / 60;
+        while (running) {
+            startTime = System.nanoTime();
+
+            canvas = null;
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized(surfaceHolder) {
+                    this.gameView.update();
+                    this.gameView.draw(canvas);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            timeMillis = (System.nanoTime() - startTime) / 1000000;
+            long sleepTime = targetTime - timeMillis;
+
+            if (sleepTime > 0) {
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
